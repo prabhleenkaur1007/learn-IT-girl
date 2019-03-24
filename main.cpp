d6378a1ec4ffe929f4ce5f6e719eab3346f5e863
Prabhleen Kaur <43867458+prabhleenkaur1007@users.noreply.github.com>
2019-03-24 15:54:55 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     int a,b;
     cout<<"enter two integers: ";
     cin>>a>>b;
-//    swap_reference(&a,&b);
+   swap_reference(&a,&b);
     swap_value(a,b);
     return 0;
 }
@@ -20,9 +20,9 @@ void swap_value(int c, int d){
     cout<<"swapped values (passby-value) are: "<<c<<" "<<d<<endl;
 }
 
-//void swap_reference(int*a,int*b){
-//    int r=*a;
-//    *a=*b;
-//    *b=r;
-//    cout<<"swapped values (passby-reference) are: "<<*a<<" "<<*b<<endl;
-//}
+void swap_reference(int*a,int*b){
+   int r=*a;
+   *a=*b;
+   *b=r;
+   cout<<"swapped values (passby-reference) are: "<<*a<<" "<<*b<<endl;
+}
